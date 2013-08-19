80378a3c6536495160e3ba3088254bd84dd729ad
Konstantin Kirik <snegovick@gmail.com>
2013-08-20 03:09:52 +0400

    added path finding and some utils, also added test maps

--- path_finding.py
+++ path_finding.py
@@ -27,8 +27,8 @@ def breadth_first_search(m_map, (x_s, y_s), (x_e, y_e)):
                 if m_map[y+yn][x+xn]==0 and tmap[y+yn][x+xn]==0:
                     new_front.append((x+xn, y+yn))
                     tmap[y+yn][x+xn] = step
-            if new_front == []:
-                return None
+        if new_front == []:
+            return None
         front = new_front
         if end in front:
             break
@@ -49,10 +49,171 @@ def breadth_first_search(m_map, (x_s, y_s), (x_e, y_e)):
             break
     return path
 
-m = [[0,0,1,0,0],
-     [0,0,1,0,0],
-     [0,0,0,0,0],
-     [0,0,1,0,0],
-     [0,0,1,0,0]]
+def scale_down(m, factor):
+    w = len(m[0])
+    h = len(m)
 
-print breadth_first_search(m, (0,0), (3,0))
+    nw = w/factor
+    nh = h/factor
+
+    nm = []
+    for y in range(nh):
+        nm.append([])
+        for x in range(nw):
+            can_pass = True
+            for i in range(-factor/2, factor/2):
+                for j in range(-factor/2, factor/2):
+                    if y*factor+i<0 or x*factor+j<0 or y*factor+i>h or x*factor+j>w:
+                        continue
+                    if m[y*factor+i][x*factor+j] == 1:
+                        nm[-1].append(1)
+                        can_pass = False
+                        break
+                if not can_pass:
+                    break
+                        
+            if can_pass:
+                nm[-1].append(0)
+    return nm
+
+def grow_margin(m, margin):
+    w = len(m[0])
+    h = len(m)
+
+    nm = []
+
+    for y, l in enumerate(m):
+        nm.append([])
+        for x, p in enumerate(l):
+            can_pass = True
+            for i in range(-margin/2, margin/2):
+                for j in range(-margin/2, margin/2):
+                    if y+i<0 or x+j<0 or y+i>=h or x+j>=w:
+                        continue
+                    if m[y+i][x+j] != 0:
+                        nm[-1].append(1)
+                        can_pass = False
+                        break
+                if not can_pass:
+                    break
+            if can_pass:
+                nm[-1].append(0)
+
+    return nm
+
+
+if __name__ == "__main__":
+    print "Test 1: Simple search"
+
+    m = [[0,0,1,0,0],
+         [0,0,1,0,0],
+         [0,0,0,0,0],
+         [0,0,1,0,0],
+         [0,0,1,0,0]]
+    
+    print "map:"
+    
+    for l in m:
+        print l
+    
+    print "path:"
+    print breadth_first_search(m, (0,0), (3,0))
+
+    print "Test 2: Big scale"
+    import Image, ImageDraw
+    im = Image.open("test_map_2.png")
+    sz = im.size
+    data = list(im.getdata())
+    map2d = []
+    for i, pt in enumerate(data):
+        if i%sz[0] == 0:
+            map2d.append([])
+        if pt == (255, 255, 255):
+            map2d[-1].append(0)
+        else:
+            map2d[-1].append(1)
+#    for l in map2d:
+#        print l
+    #print data
+    d = ImageDraw.Draw(im)
+    print sz
+    path = breadth_first_search(map2d, (0,0), (sz[0]-1, sz[1]-1))
+    print path
+    for x,y in path:
+        d.point((x, y), fill="red")
+    im.save("test_path_1.png")
+
+    print "Test 3: Downscale"
+    import Image, ImageDraw
+    im = Image.open("test_map_2.png")
+    sz = im.size
+    data = list(im.getdata())
+    map2d = []
+    for i, pt in enumerate(data):
+        if i%sz[0] == 0:
+            map2d.append([])
+        if pt == (255, 255, 255):
+            map2d[-1].append(0)
+        else:
+            map2d[-1].append(1)
+
+
+    factor = 2
+    new_map = scale_down(map2d, 2)
+    print "new_map:", new_map
+
+    im2 = Image.new("RGB", (sz[0]/factor, sz[1]/factor), "white")
+    d = ImageDraw.Draw(im2)
+    for y, l in enumerate(new_map):
+        for x, p in enumerate(l):
+            d.point((x, y), fill=("white" if p == 0 else "black"))
+    im2.save("test_downscale.png")
+
+    print "Test 4: Grow margin"
+    import Image, ImageDraw
+    im = Image.open("test_map_2.png")
+    sz = im.size
+    data = list(im.getdata())
+    map2d = []
+    for i, pt in enumerate(data):
+        if i%sz[0] == 0:
+            map2d.append([])
+        if pt == (255, 255, 255):
+            map2d[-1].append(0)
+        else:
+            map2d[-1].append(1)
+
+
+    new_map = grow_margin(map2d, 20)
+    print "new_map:", new_map
+
+    im2 = Image.new("RGB", sz, "white")
+    d = ImageDraw.Draw(im2)
+    for y, l in enumerate(new_map):
+        for x, p in enumerate(l):
+            d.point((x, y), fill=("white" if p == 0 else "black"))
+    im2.save("test_grow_margin.png")
+    
+    print "Test 5: Grow and find path"
+    import Image, ImageDraw
+    im = Image.open("test_map_2.png")
+    sz = im.size
+    data = list(im.getdata())
+    map2d = []
+    for i, pt in enumerate(data):
+        if i%sz[0] == 0:
+            map2d.append([])
+        if pt == (255, 255, 255):
+            map2d[-1].append(0)
+        else:
+            map2d[-1].append(1)
+
+    new_map = grow_margin(map2d, 20)
+    print "new_map:", new_map
+
+    path = breadth_first_search(new_map, (0,0), (sz[0]-1, sz[1]-1))
+
+    d = ImageDraw.Draw(im)
+    for x,y in path:
+        d.point((x, y), fill="red")
+    im.save("test_path_at_grown_map.png")
